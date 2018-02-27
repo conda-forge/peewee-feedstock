f2c619326cddf8041646ee03dbadd01974ae1ade
Fokko Driesprong <fokkodriesprong@godatadriven.com>
2018-02-27 17:02:44 +0100

    Add configure and build using the toolchain

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo ""
+HOME=`pwd`
+
 echo "Downloading sqlite"
 SQLITE_URL="https://www.sqlite.org/2018/"
 SQLITE_FILE="sqlite-autoconf-3220000"
@@ -9,12 +10,13 @@ curl -L -O "${SQLITE_URL}/${SQLITE_FILE}${SQLITE_EXT}"
 
 tar xvfz  ${SQLITE_FILE}${SQLITE_EXT}
 
-./${SQLITE_FILE}/configure --prefix=${PREFIX}
+cd ./${SQLITE_FILE}
+
+./configure --prefix=$PREFIX
 
 make
 make install
 
+cd $HOME
 $PYTHON setup.py build
 $PYTHON setup.py install
-
-
